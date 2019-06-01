75dbfbcaf94f6d715a63e7e07c64caee6a991f06
Fernando Calefi <fercalefi@gmail.com>
2019-06-01 19:14:19 -0300

    msg

---
+++ Contrato/Entities/Worker.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Contrato.Entities.Enums;
+
+namespace Contrato.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Departament Departament { get; set; }
+
+        // criando uma propriedade do tipo lista e já instanciando em memória
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+   
+
+
+        //construtor sem parametro
+        public Worker()
+        {
+        }
+
+        // construtor com parametros
+        public Worker(string name, WorkerLevel level, double baseSalary, Departament departament)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Departament = departament;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", "
+                + Level
+                + ", "
+                + BaseSalary;
+        }
+
+        public void addContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void removeContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double income(int year, int month)
+        {
+            foreach (HourContract obj in Contracts) 
+            {
+                if (obj.Date.Month == month && obj.Date.Year == year)
+                {
+                    BaseSalary += obj.TotalValue();
+                }
+
+
+            }
+            return BaseSalary;
+
+     
+        }
+    }
+}
---
+++ Contrato/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Globalization;
+using Contrato.Entities;
+using Contrato.Entities.Enums;
+using System.Collections.Generic;
+
+namespace Contrato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter department's name: ");
+            string nameDepartament = Console.ReadLine();
+
+            Console.WriteLine("Enter worker data: ");
+
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Level (Junior/MidLevel/Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+
+            Console.Write("Base salary: ");
+            double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // instanciando objetos
+            Departament departament = new Departament(nameDepartament);
+            Worker worker = new Worker(name, level, salary, departament);
+
+
+            Console.Write("How many contracts to this worker? ");
+            int n = int.Parse(Console.ReadLine());
+
+
+            // instanciando uma lista de contratos
+           // List<HourContract> contratoLista = new List<HourContract>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} contract data: ");
+
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Duration (hours): ");
+                int hours = int.Parse(Console.ReadLine());
+
+                // adicionando o contrato na lista
+                HourContract contrato = new HourContract(date, valuePerHour, hours);
+                worker.addContract(contrato);
+            }
+
+            Console.Write("Enter mont and year to calculate income (MM/YYYY): ");
+            string monthYear = Console.ReadLine();
+
+
+            int month = int.Parse(monthYear.Substring(0, 2));
+            //string teste = monthYear.Substring(2, 4);
+            int year = int.Parse(monthYear.Substring(3, 4));
+
+            Console.WriteLine("Name: "+ worker.Name);
+            Console.WriteLine("Departament: "+ worker.Departament.Name);
+            Console.WriteLine("Income for "+ monthYear +": "+ worker.income(year, month));
+
+
+
+
+        }
+    }
+}
